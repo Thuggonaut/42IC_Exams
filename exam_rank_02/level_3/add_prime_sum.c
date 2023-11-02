9afae05f6e4051b31c79fb7c568440b1636d385e
Thuy Quematon <thuytkq@outlook.com>
2023-11-02 20:50:17 +1030

    u

--- exam_rank_02/level_3/add_prime_sum.c
+++ exam_rank_02/level_3/add_prime_sum.c
@@ -24,22 +24,30 @@ $>
 */
 
 #include <unistd.h> //To call the write() function
+#include <stdlib.h> //To call the exit() function
 
 int ft_atoi(char *s) //Define a function that takes a string representation of a number and return an integer (the actual number)
 {
 	int	res; //Declare an integer variable to store the converted result from string to integer
-    
+    int	sign; //To check for a negative number
+
     res = 0; //Initialized to `0` because no conversion has occured yet
+	sign = 1;
+	if (*s == '-') //Check for a negative sign at the start of the string
+	{
+		sign = -1; //Set to negative
+		s++; //Move to the next character
+	}
 	while (*s) //Loop until the end of the string is reached
-		res = res * 10 + *s++ - 48; //For the current character in `s`, multiply `res` by `10`, then add the ASCII value of the current character (subtracting `48` to get the actual digit), then assign the result to res. See #1
-	return (res); //Return the actual digit of the string representation of an integer
+		res = res * 10 + *s++ - '0'; //For the current character in `s`, multiply `res` by `10`, then add the ASCII value of the current character (subtracting `48` to get the actual digit), then assign the result to res. See #1
+	return (res * sign); //Return the actual digit of the string representation of an integer
 }
 
 void    ft_putnbr(unsigned int n) //Define a function that takes an integer. The return type is `void` because the function will print the character `res` to the STDOUT
 {
 	char res; //Declare a character variable to store the converted result from integer to character representation of that integer
     
-    if (n >= 10) //Checks if `n` consists of more than `1` digit
+    if (n > 9) //Checks if `n` consists of more than `1` digit
 		ft_putnbr(n / 10); //If so, it calls itself recursively with `n / 10`, effectively removing the last digit of `n`, then passing the remaining digits to the next recursive call
 	res = (n % 10) + '0'; //Calculates the remainder of `n / 10`, effectively giving us the last digit of `n`, then add the ASCII value of `0`, effectively giving us the character representation of the digit, before assigning it to `res`
 	write(1, &res, 1); //Writes the character to the STDOUT
@@ -63,32 +71,35 @@ int is_prime(unsigned int n) //Declare a function that takes an unsigned integer
 
 void add_prime_sum(char *str) //Define a function that takes a string and prints to the STDOUT the `sum` of all prime numbers inferior or equal to a number represented by `char `str`
 {
-    unsigned int n; //Declare an unsigned integer variable to store the integer value of the number represented by a string
-    unsigned int sum; //Declare an unsigned integer variable to store the accumulated sum of the prime numbers
-    unsigned int i; //Declare an unsigned integer variable to be used as a counter in a loop
+	unsigned int n; //Declare an unsigned integer variable to store the integer value of the number represented by a string
+	unsigned int sum; //Declare an unsigned integer variable to store the accumulated sum of the prime numbers
+	unsigned int i; //Declare an unsigned integer variable to be used as a counter in a loop
 
-    n = ft_atoi(str); //Assign to `n`, the converted string to integer 
-    sum = 0; //Initialise to `0` to prepare `sum` for its accumulation of prime numbers
-    i = 2; //Initialised to `2` because the smallest prime number is `2`
-    if (is_prime(n)) //Checks if `n` itself is a prime number, if not, skip the loop
-    {
-        while (i <= n) //Loop from `2` until `n`
-        {
-            if (is_prime(i)) //Checks if `i` is prime
-                sum += i; //If so, add `i`'s value to `sum`
-            i++; //Move to the next number between `2` and `n` to check for potential primes before adding it's value to `sum`
-        } 
-    }
-    if (sum != 0) //Checks if `sum` is not `0`
-        ft_putnbr(sum); //If not `0`, write `sum` to the STDOUT, otherwise nothing is printed
+	n = ft_atoi(str); //Assign to `n`, the converted string to integer 
+	sum = 0; //Initialise to `0` to prepare `sum` for its accumulation of prime numbers
+	i = 2; //Initialised to `2` because the smallest prime number is `2`
+	while (i <= n) //Loop from `2` until `n`
+	{
+		if (is_prime(i)) //Checks if `i` is prime
+			sum += i; //If so, add `i`'s value to `sum`
+		i++; //Move to the next number between `2` and `n` to check for potential primes before adding it's value to `sum`
+	}
+	ft_putnbr(sum); //Write the accumulated `sum` to the STDOUT
 }
 
 int main(int argc, char **argv) //`argv` is a pointer to an array of strings. By using a double pointer `char **argv`, the program can access and retrieve each individual argument as a null-terminated string
 {
-    if (argc == 2) //Perform operations only if argc is two, meaning there needs to be 1argument in addition to the program name
-        add_prime_sum(argv[1]);
-    write(1, "\n", 1);
-    return (0);
+	if (argc != 2 || ft_atoi(argv[1]) < 0) //Check if argument is not 1, or if input is a negative value
+	{
+		write(1, "0\n", 2);
+		exit(0); //Stop executing further
+	}
+	if (argc == 2) //Perform operations only if argc is two, meaning there needs to be 1 argument in addition to the program name
+	{
+		add_prime_sum(argv[1]);
+		write(1, "\n", 1);
+	}
+	return (0);
 }
 
 
@@ -96,10 +107,10 @@ int main(int argc, char **argv) //`argv` is a pointer to an array of strings. By
 PRIME NUMBERS:
 
 1.  A prime number is a natural number greater than `1` that has no positive divisors other than `1` and itself. 
-    - In other words, a prime number is a number that cannot be formed by multiplying two smaller natural numbers. 
-    - The opposite of `prime` is `composite`.
-    - The first few prime numbers are `2`, `3`, `5`, `7`, `11`, and `13`.
-    - https://www.youtube.com/watch?v=klcIklsWzrY 
+	- In other words, a prime number is a number that cannot be formed by multiplying two smaller natural numbers. 
+	- The opposite of `prime` is `composite`.
+	- The first few prime numbers are `2`, `3`, `5`, `7`, `11`, and `13`.
+	- https://www.youtube.com/watch?v=klcIklsWzrY 
 
 
 SIEVE OF ERATOSTHENES ALGORITHM:
@@ -108,16 +119,16 @@ SIEVE OF ERATOSTHENES ALGORITHM:
 
 2.  To implement the Sieve of Eratosthenes algorithm in C, you can follow these steps:
 
-    a. Declare an array of size `num-1` to store the numbers from `2` to `num`. 
-            - Initialize all elements of the array with their respective numbers using a while loop.
+	a. Declare an array of size `num-1` to store the numbers from `2` to `num`. 
+			- Initialize all elements of the array with their respective numbers using a while loop.
 
-    b. Use a while loop to iterate through the array and cross out the non-prime numbers. 
-            - Start with the first prime number, which is `2`, and mark all its multiples as `0`. 
-            - Move on to the next number that is not crossed out, which is a prime, and repeat the process.
+	b. Use a while loop to iterate through the array and cross out the non-prime numbers. 
+			- Start with the first prime number, which is `2`, and mark all its multiples as `0`. 
+			- Move on to the next number that is not crossed out, which is a prime, and repeat the process.
 
-    c. Print only the prime numbers from the array using a while loop.
+	c. Print only the prime numbers from the array using a while loop.
 
-    d. Here's an example implementation:
+	d. Here's an example implementation:
 
 #include <stdio.h>
 #include <math.h>
@@ -125,71 +136,71 @@ SIEVE OF ERATOSTHENES ALGORITHM:
 
 void sieveOfEratosthenes(int num) 
 {
-    int sieve[num-1];
-    int index = 0;
-    int fillnum = 2;
+	int sieve[num-1];
+	int index = 0;
+	int fillnum = 2;
 
-    while (fillnum <= num) 
-    {
-        sieve[index] = fillnum;
-        index++;
-        fillnum++;
-    }
+	while (fillnum <= num) 
+	{
+		sieve[index] = fillnum;
+		index++;
+		fillnum++;
+	}
 
-    int primenums = 0;
-    while (primenums < sqrt(num)) 
-    {
-        if (sieve[primenums] != 0) 
-        {
-            int multiple = sieve[primenums];
-        
-            while (multiple < num) 
-            {
-                if (sieve[multiple] != 0) 
-                {
-                    sieve[multiple] = 0;
-                }
-                multiple += sieve[primenums];
-            }
-        }
-        primenums++;
-    }
+	int primenums = 0;
+	while (primenums < sqrt(num)) 
+	{
+		if (sieve[primenums] != 0) 
+		{
+			int multiple = sieve[primenums];
+		
+			while (multiple < num) 
+			{
+				if (sieve[multiple] != 0) 
+				{
+					sieve[multiple] = 0;
+				}
+				multiple += sieve[primenums];
+			}
+		}
+		primenums++;
+	}
 
-    int k = 0;
-    while (k < num-1) 
-    {
-        if (sieve[k] != 0) 
-        {
-            printf("%d ", sieve[k]);
-        }
-        k++;
-    }
+	int k = 0;
+	while (k < num-1) 
+	{
+		if (sieve[k] != 0) 
+		{
+			printf("%d ", sieve[k]);
+		}
+		k++;
+	}
 }
 
 int main() 
 {
-    int num = 1000;
-    sieveOfEratosthenes(num);
-    return 0;
+	int num = 1000;
+	sieveOfEratosthenes(num);
+	return 0;
 }
 
 #1  Re ft_atoi(), Let's break down the line `res = res * 10 + *s++ - 48;`, which is executed until the end of `s` is reached:
-    a.  `res * 10`:
-        - The current value of res is multiplied by 10. This is done because each digit in a number is ten times more significant than the digit to its right. 
-        - So, to make space for the next digit, we multiply res by 10.
-    b. `*s++`: 
-        - This is a bit trickier. It's using pointer arithmetic and the post-increment operator. 
-        - The `*s` part is dereferencing the pointer, getting the character that `s` currently points to. 
-        - The `++` part increments the pointer `s` to the next character in the string after the value is fetched. 
-        - So this whole expression fetches the current character in the string and then moves the pointer to the next character.
-    c.  `- 48`: 
-        - The characters '0' through '9' in ASCII are represented by the numbers `48` through `57`, so subtracting `48` from an ASCII character yields the 
-          integer value of a number character. 
-        - This is done to convert the character digit to its actual integer value.
-    d.  Example if *s = 123:
-        - Initially, res = 0.
-        - We fetch the first char, which is '1', its ASCII value minus `48` is `1`. So `res = res * 10 + 1` --> `res` is now `1`.
-        - We fetch the second char, which is '2'. So `res = res * 10 + 2` --> `res` is now `12`.
-        - We fetch the third char, which is '3'. So `res = res * 10 + 3` --> `res` is now `123`.
-        - After the last digit, there are no more characters, so we stop there and `res` is the integer equivalent of the string.
+	a.  `res * 10`:
+		- The current value of res is multiplied by 10. This is done because each digit in a number is ten times more significant than the digit to its right. 
+		- So, to make space for the next digit, we multiply res by 10.
+	b. `*s++`: 
+		- This is a bit trickier. It's using pointer arithmetic and the post-increment operator. 
+		- The `*s` part is dereferencing the pointer, getting the character that `s` currently points to. 
+		- The `++` part increments the pointer `s` to the next character in the string after the value is fetched. 
+		- So this whole expression fetches the current character in the string and then moves the pointer to the next character.
+	c.  `- 48`: 
+		- The characters '0' through '9' in ASCII are represented by the numbers `48` through `57`, so subtracting `48` from an ASCII character yields the 
+		  integer value of a number character. 
+		- This is done to convert the character digit to its actual integer value.
+	d.  Example if *s = 123:
+		- Initially, res = 0.
+		- We fetch the first char, which is '1', its ASCII value minus `48` is `1`. So `res = res * 10 + 1` --> `res` is now `1`.
+		- We fetch the second char, which is '2'. So `res = res * 10 + 2` --> `res` is now `12`.
+		- We fetch the third char, which is '3'. So `res = res * 10 + 3` --> `res` is now `123`.
+		- After the last digit, there are no more characters, so we stop there and `res` is the integer equivalent of the string.
 */
---
+++ exam_rank_02/level_3/hidenp.c
@@ -0,0 +1,56 @@
+/*
+Assignment name  : hidenp
+Expected files   : hidenp.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program named hidenp that takes two strings and displays 1
+followed by a newline if the first string is hidden in the second one,
+otherwise displays 0 followed by a newline.
+
+Let s1 and s2 be strings. We say that s1 is hidden in s2 if it's possible to
+find each character from s1 in s2, in the same order as they appear in s1.
+Also, the empty string is hidden in any string.
+
+If the number of parameters is not 2, the program displays a newline.
+
+Examples :
+
+$>./hidenp "fgex.;" "tyf34gdgf;'ektufjhgdgex.;.;rtjynur6" | cat -e
+1$
+$>./hidenp "abc" "2altrb53c.sse" | cat -e
+1$
+$>./hidenp "abc" "btarc" | cat -e
+0$
+$>./hidenp | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+void	hidenp(char *s1, char *s2) 
+{
+	while (*s2) 
+	{
+		if (*s1 == *s2) 
+		{
+			s1++; // Move to the next character in s1
+			if (*s1 == '\0') 
+			{
+				write(1, "1", 1); // If end of s1 is reached, all characters have been found in order
+				return ;
+			}
+		}
+		s2++; // Always move to the next character in s2
+	}
+	write(1, "0", 1); // If the loop ends, s1 is not fully found in s2 in order
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+		hidenp(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
+}
